c45edfe961f15bc76d6ead2a43b66789afcdafce
cywwee <kyrylo1buryk.it@gmail.com>
2025-04-15 18:21:31 +0200

    added: array sorting program and program to find the max value in the array

---
+++ PRG/mini-programs/max-value-in-arr.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Zadejte delku pole: ");
+int delkaPole = int.Parse(Console.ReadLine());
+
+int[] pole = new int[delkaPole];
+
+
+for (int i = 0; i < delkaPole; i++)
+{
+    Console.Write($"Zadejte čislo na pozici {i + 1}: ");
+    pole[i] = int.Parse(Console.ReadLine());
+
+}
+
+
+int max = pole[0];
+
+for (int i = 1; i < delkaPole; i++)
+{
+
+    if (pole[i] > max)
+    {
+        max = pole[i];
+    }
+
+}
+
+
+Console.WriteLine($"Největši čislo v poli je: {max}");
---
+++ PRG/mini-programs/sort-arr.cs
@@ -0,0 +1,17 @@
+﻿int[] numbers = { 5, 2, 8, 1, 3 };
+
+Console.WriteLine("Původní pole:");
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write(numbers[i] + " ");
+}
+Console.WriteLine();
+
+Array.Sort(numbers);
+
+Console.WriteLine("Seřazené pole:");
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write(numbers[i] + " ");
+}
+Console.WriteLine();
